7d69a3fd7c344860270fc1a56e2bb5926da90f8c
Sava Mihajlov <79896945+Savaxc@users.noreply.github.com>
2024-05-04 14:39:33 +0200

    Initial commit with project template

---
+++ aprcroso/src/main/java/com/backend/aprcroso/AprcrosoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.aprcroso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprcrosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprcrosoApplication.class, args);
+	}
+
+}
---
+++ aprcroso/src/test/java/com/backend/aprcroso/AprcrosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.aprcroso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprcrosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
