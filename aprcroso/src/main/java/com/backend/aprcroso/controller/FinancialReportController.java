9778c24f59c36930100e95546063289a20dc9da8
Sava Mihajlov <79896945+Savaxc@users.noreply.github.com>
2025-01-08 18:24:25 +0100

    Financial-report api update

--- aprcroso/src/main/java/com/backend/aprcroso/controller/FinancialReportController.java
+++ aprcroso/src/main/java/com/backend/aprcroso/controller/FinancialReportController.java
@@ -1,14 +1,14 @@
 package com.backend.aprcroso.controller;
 
+import com.backend.aprcroso.exception.NotFoundException;
 import com.backend.aprcroso.model.FinancialReport;
 import com.backend.aprcroso.service.FinancialReportService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/financial-reports")
@@ -25,5 +25,24 @@ public class FinancialReportController {
     public ResponseEntity<FinancialReport> submitFinancialReport(@Valid @RequestBody FinancialReport financialReport) {
         FinancialReport savedReport = financialReportService.saveFinancialReport(financialReport);
         return ResponseEntity.ok(savedReport);
+    }
+
+    //Getting By Id
+    @GetMapping("/{id}")
+    public ResponseEntity<FinancialReport> getFinancialReportById(@PathVariable Long id) {
+        FinancialReport financialReport = financialReportService.getFinancialReportById(id);
+        if (financialReport != null) {
+            return ResponseEntity.ok(financialReport);
+        } else {
+            throw new NotFoundException("Financial Report with selected ID not found.");
+//            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //gettin All Financial-reports List
+    @GetMapping
+    public ResponseEntity<List<FinancialReport>> getAllFinancialReports() {
+        List<FinancialReport> financialReports = financialReportService.getAllFinancialReports();
+        return ResponseEntity.ok(financialReports);
     }
 }
--- aprcroso/src/main/java/com/backend/aprcroso/service/FinancialReportService.java
+++ aprcroso/src/main/java/com/backend/aprcroso/service/FinancialReportService.java
@@ -5,6 +5,8 @@ import com.backend.aprcroso.repository.FinancialReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class FinancialReportService {
 
@@ -15,7 +17,17 @@ public class FinancialReportService {
         this.financialReportRepository = financialReportRepository;
     }
 
+    public List<FinancialReport> getAllFinancialReports() {
+        return financialReportRepository.findAll();
+    }
+
+    public FinancialReport getFinancialReportById(Long id) {
+        return financialReportRepository.findById(id).orElse(null);
+    }
+
     public FinancialReport saveFinancialReport(FinancialReport financialReport) {
         return financialReportRepository.save(financialReport);
     }
+
+
 }
