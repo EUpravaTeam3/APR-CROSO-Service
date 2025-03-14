32bec7f6a4ae0b4d4340549ebec7f513371ac28f
Sava Mihajlov <79896945+Savaxc@users.noreply.github.com>
2025-03-14 18:21:53 +0100

    Search companies added

--- front-end/apr-croso-frontend/src/app/components/user/company-list/company-list.component.ts
+++ front-end/apr-croso-frontend/src/app/components/user/company-list/company-list.component.ts
@@ -12,11 +12,42 @@ export class CompanyListComponent {
 
   companies: Company[] = [];
 
+  nameFilter: String = '';
+  pibFilter: String = '';
+  regNumberFilter: String = '';
+  regDateFilter: String = '';
+  statusFilter: String = ''; // Ostaviti prazan string kao podrazumevano (sve kompanije prikazane)
+
+  statusOptions: String[] = ['ACTIVE', 'INACTIVE', 'DELETED', 'BANKRUPTCY', 'LIQUIDATION'];
+
   constructor(private companyService: CompanyService, private router: Router){}
 
   ngOnInit(): void {
     this.getCompanies();
   }
+
+  matchesSearch(fieldValue: String, filterValue: String): boolean {
+    if (!filterValue.trim()) return true;
+    return fieldValue.toLowerCase().includes(filterValue.toLowerCase());
+  }
+
+  get filteredCompanies() {
+    return this.companies.filter(company =>
+      this.matchesSearch(company.name, this.nameFilter) &&
+      this.matchesSearch(company.pib, this.pibFilter) &&
+      this.matchesSearch(company.registrationNumber, this.regNumberFilter) &&
+      (this.statusFilter ? company.companyStatus === this.statusFilter : true) // TAČNA PODUDARNOST ZA STATUS
+    );
+  }
+
+  resetFilters() {
+    this.nameFilter = '';
+    this.pibFilter = '';
+    this.regNumberFilter = '';
+    this.regDateFilter = '';
+    this.statusFilter = '';
+  }
+
 
   private getCompanies(){
     this.companyService.getCompanyList().subscribe(data => {
